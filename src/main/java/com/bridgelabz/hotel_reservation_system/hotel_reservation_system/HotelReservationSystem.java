10ae6afb8c531728e7c7414149f7b8ed913a6ed1
tushar.kasturi_ymedi <tusharkasturi1999@gmail.com>
2021-09-22 08:23:20 +0530

    Initial Commit

---
+++ src/main/java/com/bridgelabz/hotel_reservation_system/hotel_reservation_system/HotelReservationSystem.java
@@ -0,0 +1,13 @@
+package com.bridgelabz.hotel_reservation_system.hotel_reservation_system;
+
+/**
+ * Hello world!
+ *
+ */
+public class HotelReservationSystem 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Welcome to Hotel Reservation System" );
+    }
+}
